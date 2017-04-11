ef73cd5995d39beb393bded60b91ad9924a494a3
Jaemin <jaeminlime@gmail.com>
2017-04-10 20:16:31 -0400

    wheel direction changes based on roll

--- master.ino
+++ master.ino
@@ -1,9 +1,7 @@
 //Filter declarations
 #include <MovingAverageFilter.h>
 MovingAverageFilter movingAverageFilter(100);
-float filtered_final_counter_speed = 0;
-float input = 0;
-float filtered_filter = 0;
+MovingAverageFilter movingAverageFilter2(100);
 
 //Time Declerations
 #include <FlexiTimer2.h>
@@ -41,7 +39,6 @@ VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measure
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
 // Interrupt Deteton Routine
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
@@ -51,62 +48,8 @@ double yaw;
 double pitch;
 double roll;
 
-//Angular Velocity Declerations
-double oldTheta = 0;
-double newTheta = 0;
-double oldThetaM = 0;
-double newThetaM = 0;
-float oldTime = 0;
-float newTime = 0;
-float angVelMain;
-float angVelMotor;
-float omega_initial = 0;
-float omega_final = 0;
-float omega_final_unfiltered = 0;
-float omega_inital_speed = 0;
-float omega_final_speed = 0;
-unsigned long time_prev = 0;
-unsigned long time_now = 0;
-double omega;
-#define omega_Filter 0.7
-#define omega_Speed_Filter 0.7
-#define angle_Rounding_Value 1000.
-
-void get_omega() {
-  time_prev = time_now;
-  time_now = micros();
-
-  omega_initial = omega_final;
-  omega_final_unfiltered = round((-ypr[2] * 180 / M_PI) * angle_Rounding_Value) / angle_Rounding_Value;
-  omega_final = (1 - omega_Filter) * (omega_final_unfiltered) + omega_Filter * (omega_initial);
-  omega_inital_speed = omega_final_speed;
-  omega_final_speed = ((1 - omega_Speed_Filter) * (omega_final - omega_initial) / (time_now - time_prev) * 1000000.) + omega_Speed_Filter * omega_inital_speed;
-}
-
-
-float counter_initial = 0;
-float counter_final = 0;
-float counter_initial_speed = 0;
-float counter_final_speed = 0;
-
-void get_rpm(){
-  time_prev = time_now;
-  time_now = micros();
-
-  counter_initial = counter_final;
-  counter_final = counter;
-  counter_final_speed = counter_initial_speed;
-  counter_final_speed = ((counter_final - counter_initial) / ((time_now - time_prev)*0.000001));
-}
-
-
-
-
-
 void flash(){
-  setMotor(100);
-  getEncoder();
-  
+  getEncoder(); 
 }
 
 void setup(){
@@ -133,8 +76,6 @@ void setup(){
         Fastwire::setup(400, true);
     #endif
     // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
     Serial.begin(115200);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
     // initialize device
@@ -144,8 +85,7 @@ void setup(){
     // verify connection
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-    // wait for ready
-   
+    // wait for ready  
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
@@ -179,52 +119,76 @@ void setup(){
     }
 }
 
+double voltage = 0;
+float setAngle = 2.56;
+long int pwm;
+
 void loop(){
-  get_omega();
+  if (roll > setAngle){
+    setReverseMotor(voltage);
+  } else if (roll < setAngle){
+    setForwardMotor(voltage);
+  }
   getAngles();
-  get_rpm();
-  filtering();
+  float omega = get_AngVel();
+  float cps = get_AngVelMotor();
+  omega = movingAverageFilter2.process(omega);
+  omega = movingAverageFilter2.process(omega);
+  cps = movingAverageFilter.process(cps);
+  cps = movingAverageFilter.process(cps);
   float seconds = micros()*0.000001;
-  Serial.print("time: ");
   Serial.print(seconds);
+  Serial.print(",");
   Serial.print("\t");
-  Serial.print("roll: ");
   Serial.print(roll);
+  Serial.print(",");
   Serial.print("\t");
-  Serial.print("counter: ");
   Serial.print(counter);
+  Serial.print(",");
   Serial.print("\t");
-  Serial.print("omega: ");
-  Serial.print(omega_final_speed);
+  Serial.print(omega);
+  Serial.print(",");
   Serial.print("\t");
-  Serial.print("RPM ");
-  Serial.println(filtered_filter);
-
+  Serial.println(cps);
 }
 
-void filtering(){
-  input = counter_final_speed;
-  filtered_final_counter_speed = movingAverageFilter.process(input);
-  filtered_filter = movingAverageFilter.process(filtered_final_counter_speed);
+void setForwardMotor(double val){
+  pwm = val*(255/19);
+  if (pwm >= 255){ pwm = 255; }
+  else if (pwm <= 0) { pwm = 0; }
+  digitalWrite(set1, LOW);
+  digitalWrite(set2, HIGH);
+  analogWrite(enable, pwm);
 }
 
+void setReverseMotor(double val){
+  long int pwm = val*(255/19);
+  if (pwm >= 255){ pwm = 255; }
+  else if (pwm <= 0) { pwm = 0; }
+  digitalWrite(set1, HIGH);
+  digitalWrite(set2, LOW);
+  analogWrite(enable, pwm);
+}
+
+void getEncoder(){
+  aState = digitalRead(outputA); // Reads the "current" state of the outputA
+  // If the previous and the current state of the outputA are different, that means a Pulse has occured
+  if (aState != aLastState){     
+    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+    if (digitalRead(outputB) != aState) { 
+      counter ++;
+    } else {
+      counter --;
+    }
+  } 
+  aLastState = aState; // Updates the previous state of the outputA with the current state
+}
 
 void getAngles(){
   // if programming failed, don't try to do anything
     if (!dmpReady) return;
     // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-    }
+    while (!mpuInterrupt && fifoCount < packetSize) {}
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
@@ -252,32 +216,45 @@ void getAngles(){
             yaw = ypr[0] * 180/M_PI;
             pitch = ypr[1] * 180/M_PI;
             roll = ypr[2] * 180/M_PI;
-//            Serial.print("ypr\t");
-//            Serial.print(yaw);
-//            Serial.print("\t");
-//            Serial.print(pitch);
-//            Serial.print("\t");
-//            Serial.println(roll);
         #endif
     }
 }
 
-void setMotor(int val){
-  digitalWrite(set1, LOW);
-  digitalWrite(set2, HIGH);
-  analogWrite(enable, val);
+float omega_initial = 0;
+float omega_final = 0;
+float omega_final_unfiltered = 0;
+float omega_inital_speed = 0;
+float omega_final_speed = 0;
+unsigned long time_prev = 0;
+unsigned long time_now = 0;
+double omega;
+#define omega_Filter 0.7
+#define omega_Speed_Filter 0.7
+#define angle_Rounding_Value 1000.
+
+float get_AngVel() {
+  time_prev = time_now;
+  time_now = micros();
+
+  omega_initial = omega_final;
+  omega_final_unfiltered = round((-ypr[2] * 180 / M_PI) * angle_Rounding_Value) / angle_Rounding_Value;
+  omega_final = (1 - omega_Filter) * (omega_final_unfiltered) + omega_Filter * (omega_initial);
+  omega_inital_speed = omega_final_speed;
+  omega_final_speed = ((1 - omega_Speed_Filter) * (omega_final - omega_initial) / (time_now - time_prev) * 1000000.) + omega_Speed_Filter * omega_inital_speed;
+  return omega_final_speed;
 }
 
-void getEncoder(){
-  aState = digitalRead(outputA); // Reads the "current" state of the outputA
-  // If the previous and the current state of the outputA are different, that means a Pulse has occured
-  if (aState != aLastState){     
-    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-    if (digitalRead(outputB) != aState) { 
-      counter ++;
-    } else {
-      counter --;
-    }
-  } 
-  aLastState = aState; // Updates the previous state of the outputA with the current state
+float counter_initial = 0;
+float counter_final = 0;
+float counter_initial_speed = 0;
+float counter_final_speed = 0;
+
+float get_AngVelMotor(){
+  time_prev = time_now;
+  time_now = micros();
+
+  counter_initial = counter_final;
+  counter_final = counter;
+  counter_final_speed = ((counter_final - counter_initial) / ((time_now - time_prev)*0.000001));
+  return counter_final_speed;
 }
